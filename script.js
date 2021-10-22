ca5f521cad5a072ba59683d41ae47f7483275c61
Juan Espinoza <espinozaajuan04@gmail.com>
2021-10-22 11:09:29 -0300

    Fetch API to get random quotes

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('testing');
+let apiQuotes = [];
+
+// show new quote
+function newQuote() {
+   // pick a random quote from apiQuotes array
+   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]; 
+   console.log(quote);
+}
+
+// get quotes from api
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        //Catch error
+    }
+}
+
+// on load
+getQuotes();
